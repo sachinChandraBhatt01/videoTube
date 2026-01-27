5179118dc21e1a3e26f26aa8e5b4d20a5c3f4530
Sachin Chandra Bhatt <sachin51565@gmail.com>
2026-01-27 16:14:54 +0530

    first commit

---
+++ frontend/src/customHooks/UseGetHistroy.jsx
@@ -0,0 +1,43 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setVideoHistory, setShortHistory } from "../redux/userSlice"; 
+import axios from "axios";
+import { serverUrl } from "../App";
+
+const UseGetHistory = () => {
+  const dispatch = useDispatch();
+
+  const { historyRefresh } = useSelector((state) => state.user); // Ensure user state is available
+
+  useEffect(() => {
+    const fetchHistory = async () => {
+      try {
+        const result = await axios.get(serverUrl + "/api/user/gethistory", {
+          withCredentials: true,
+        });
+        console.log("🔄 Fetching history from server..." , result);
+        const history = result.data || [];
+
+        // ✅ Split videos & shorts
+        const videos = history.filter(item => item.contentType === "Video");
+        const shorts = history.filter(item => item.contentType === "Short");
+
+        // Redux dispatch
+        dispatch(setVideoHistory(videos));
+        dispatch(setShortHistory(shorts));
+
+        console.log("📺 History fetched:", { videos, shorts });
+      } catch (error) {
+        console.error("❌ Error fetching history:", error);
+
+        // Agar error ho to empty history
+        dispatch(setVideoHistory([]));
+        dispatch(setShortHistory([]));
+      }
+    };
+
+    fetchHistory();
+  }, [dispatch , historyRefresh]); // Re-fetch when historyRefresh changes
+};
+
+export default UseGetHistory;  
---
+++ frontend/src/customHooks/UseGetRecommendation.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { serverUrl } from "../App";
+import axios from "axios";
+import { setRecommendationData } from "../redux/contentSlice";
+
+const UseGetRecommendation = () => {
+  const dispatch = useDispatch();
+ const { recommendationRefresh } = useSelector(
+    (state) => state.content
+  );
+  useEffect(() => {
+    const fetchRecommendation = async () => {
+      try {
+        const res = await axios.get(
+          `${serverUrl}/api/user/getrecommendation`,
+          { withCredentials: true }
+        );
+        dispatch(setRecommendationData(res.data))
+        console.log("Recommendation data:", res.data);
+      } catch (err) {
+        console.error("Recommendation fetch error:", err);
+      }
+    };
+
+    fetchRecommendation();
+  }, [dispatch , recommendationRefresh]);
+
+  return null; // hook kuch render nahi karega
+};
+
+export default UseGetRecommendation;
