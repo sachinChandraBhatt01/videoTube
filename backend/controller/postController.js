d8a1470ff57790abec7fb0a022f20130df0d2456
Sachin Chandra Bhatt <sachin51565@gmail.com>
2026-01-31 17:09:18 +0530

    change

--- backend/controller/postController.js
+++ backend/controller/postController.js
@@ -1,9 +1,6 @@
 import Channel from "../model/channelModel.js";
 import uploadOnCloudinary from "../config/cloudinary.js";
-import Post from "../model/PostModel.js";
-
-
-
+import Post from "../model/postModel.js";
 export const createPost = async (req, res) => {
   try {
     const { channelId, content } = req.body;
